889bc67855ba6e2f051706957c23fdf4d51c7fa4
Felienne <felienne@gmail.com>
2022-05-16 20:46:51 +0200

    Revert "Translations update from Hosted Weblate (#2740)"
    This reverts commit f818a19a7b4c8ee5628bb21bea7624de0f0465ae.

--- hedy.py
+++ hedy.py
@@ -1629,15 +1629,9 @@ class ConvertToPython_12(ConvertToPython_11):
     def number(self, args):
         # try all ints? return ints
         try:
-            all_int = [str(int(x)) == x for x in args]
-            if all(all_int):
+            if all([str(int(x)) == x for x in args]):
                 return ''.join(args)
-            else:
-                # int works (=does not throw) but returns a different number? these are non-latin numerals
-                # cast and move on
-                return ''.join(str(int(x)) for x in args)
-
-        except Exception as E:
+        except:
             # if not? make into all floats
             numbers = [str(float(x)) for x in args]
             return ''.join(numbers)
